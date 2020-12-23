4da37160d0a0fe06ddd12f343171b6495d26edc3
Thijs van Dijk <github@inurbanus.nl>
2020-12-23 16:00:13 +0100

    Actually throw 404's instead of showing the home page

--- qm9k/handlers/home.go
+++ qm9k/handlers/home.go
@@ -6,9 +6,17 @@ type emptyRequest struct{}
 
 func (emptyRequest) FlaggedAsRequest() {}
 
+type homeRequest struct {
+	Path string
+}
+
+func (homeRequest) FlaggedAsRequest() {}
+
 // HomeDecoder decodes a request for the home page
-func HomeDecoder(*http.Request) (Request, error) {
-	return emptyRequest{}, nil
+func HomeDecoder(r *http.Request) (Request, error) {
+	return homeRequest{
+		Path: r.URL.Path,
+	}, nil
 }
 
 type homeResponse struct {
@@ -17,9 +25,21 @@ type homeResponse struct {
 func (homeResponse) FlaggedAsResponse() {}
 
 // HomeHandler handles requests for the home page
-func HomeHandler(s State, _ Request) (State, Response, error) {
-	if s.User.Nick == "" {
-		return s, homeResponse{}, errRedirect{"profile?new=1"}
+func HomeHandler(s State, r Request) (State, Response, error) {
+	var rv homeResponse
+
+	req, ok := r.(homeRequest)
+	if !ok {
+		return s, rv, errWrongRequestType{}
 	}
-	return s, homeResponse{}, nil
+
+	if req.Path != "/" {
+		return s, rv, errNotFound("", "")
+	}
+
+	if s.User.Nick == "" {
+		return s, rv, errRedirect{"profile?new=1"}
+	}
+
+	return s, rv, nil
 }
